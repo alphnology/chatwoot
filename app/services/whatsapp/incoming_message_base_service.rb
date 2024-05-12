df9c1777688d8856bbb2e9b704a510d4dcdb5669
Hector Ventura <hectorvent@gmail.com>
2024-05-12 14:41:23 -0500

    Adding message_id on whatsapp interative message for bot

--- app/services/whatsapp/incoming_message_base_service.rb
+++ app/services/whatsapp/incoming_message_base_service.rb
@@ -148,7 +148,8 @@ class Whatsapp::IncomingMessageBaseService
       message_type: :incoming,
       sender: @contact,
       source_id: message[:id].to_s,
-      in_reply_to_external_id: @in_reply_to_external_id
+      in_reply_to_external_id: @in_reply_to_external_id,
+      in_reply_to_interactive_id: @in_reply_to_interactive_id
     )
   end
 
